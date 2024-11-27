cf523c6ad160833b85200070f4defb450d1f6db0
Andria Pratama <andrilocong1st@gmail.com>
2024-11-28 07:27:20 +0800

    [#1] add logic in sales order

---
+++ src/app/@entities/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+  public id: number;
+  public name: string;
+  public category: string;
+  public amount: number;
+  public price: number;
+  public description: string;
+  public note: string;
+}
--- src/app/sales/order/order.component.ts
+++ src/app/sales/order/order.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../../@entities/product';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { CommonModule } from '@angular/common';
@@ -14,7 +15,9 @@ import { SalesService } from '../sales.service';
   styleUrl: './order.component.scss',
 })
 export class OrderComponent implements OnInit {
-  public productList: string[] = [];
+  public productSelected: Product = new Product();
+  public isShowProductDetailModal: boolean = false;
+
   constructor(
     public salesSvc: SalesService,
     public dummySvc: DummyService,
@@ -24,11 +27,72 @@ export class OrderComponent implements OnInit {
     setTimeout(() => {
       this.salesSvc.isTemplate = false;
     });
+  }
 
-    for (let i = 0; i < 20; i++) {
-      this.productList.push('');
+  public onShowProductDetailModal(product: Product): void {
+    this.isShowProductDetailModal = true;
+    this.productSelected = product;
+  }
+
+  public onHideProductDetailModal(): void {
+    this.isShowProductDetailModal = false;
+    setTimeout(() => {
+      this.productSelected = new Product();
+    }, 200);
+  }
+
+  public formatPrice(price: number, style: 'short' | 'full'): string {
+    if (style === 'short') {
+      if (price >= 1000) {
+        const newFormat = (price / 1000).toFixed(0) + 'K';
+        return `IDR ${newFormat}`;
+      }
+      return `IDR ${price.toString()}`;
+    } else {
+      const newFormat = new Intl.NumberFormat().format(price);
+      return `IDR ${newFormat}`;
     }
+  }
 
-    console.log(this.productList);
+  public setCategoryBgColor(category: string) {
+    switch (category) {
+      case 'breads':
+        return 'bg-[#f97316]/10';
+      case 'cakes':
+        return 'bg-[#22c55e]/10';
+      case 'donuts':
+        return 'bg-[#06b6d4]/10';
+      case 'pastries':
+        return 'bg-[#6366f1]/10';
+      case 'sandwich':
+        return 'bg-[#d946ef]/10';
+      case 'coffee':
+        return 'bg-[#f43f5e]/10';
+      case 'non-coffee':
+        return 'bg-[#eab308]/10';
+      default:
+        return 'bg-[#737373]/10';
+    }
+  }
+
+  public setCategoryTextColor(category: string) {
+    switch (category) {
+      case 'breads':
+        return 'text-[#f97316]';
+      case 'cakes':
+        return 'text-[#22c55e]';
+      case 'donuts':
+        return 'text-[#06b6d4]';
+      case 'pastries':
+        return 'text-[#6366f1]';
+      case 'sandwich':
+        return 'text-[#d946ef]';
+      case 'coffee':
+        return 'text-[#f43f5e]';
+      case 'non-coffee':
+        return 'text-[#eab308]';
+      default:
+        return 'text-[#737373]';
+    }
   }
 }
